798ba25367230200942ab3361bdae5d1352e35e9
Aman Rajbhar <{104845206}+{chrollo007}@users.noreply.github.com>
2024-12-31 19:34:32 +0530

    fix the grid size and scrolling

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const temp=document.querySelector(".container")
+const gridsize=16;
+const containersize=500;
+temp.style.width=`${containersize}px`
+temp.style.height=`${containersize}px`
 
-for(let i=1;i<16*16;i++){
+const size=500/gridsize;
+
+for(let i=0;i<gridsize*gridsize;i++){
         const div=document.createElement("div")
         div.classList.add("grid")
+        div.style.width=`${size}px`;
+        div.style.height=`${size}px`;
         temp.appendChild(div)
 }
